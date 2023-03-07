7a3b86ae3bd33b15aa06c55356fd27c2f7d17c88
Nick Johnson <nlj222@gmail.com>
2023-03-06 19:03:05 -0800

    lab complete

---
+++ lib/migraations/versions/1c13d95aecbc_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 1c13d95aecbc
+Revises: 
+Create Date: 2023-03-06 18:56:42.648069
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1c13d95aecbc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
